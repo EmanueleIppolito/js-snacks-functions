efe5841949acf685246b62a5752a785ac2727add
EmanueleIppolito <emanuele2410@gmail.com>
2025-07-18 20:53:38 +0200

    end ex2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+/*function getInitials(namesArray) {
+    const initials = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        const name = namesArray[i];
+        initials.push(name.charAt(0));
+    }
+    return initials;
+}*/
+
+const iniziali = (namesArray) => {
+    const initials = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        const name = namesArray[i];
+        initials.push(name.charAt(0));
+    }
+    return initials;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initials)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
